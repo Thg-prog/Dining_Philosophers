72dc77636e0086205361ad3c27546c0f2ac37271
Thg-prog <84495646+Thg-prog@users.noreply.github.com>
2022-12-05 22:26:31 +0300

    Initial Commit

---
+++ Five-Dining_philosophers/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Five-Dining_philosophers
+//
+//  Created by Антон on 05.12.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
